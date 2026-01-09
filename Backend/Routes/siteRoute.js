595e87567744a9390e0209298f8abb8e2373a143
Parth-Chikalkar <parthchikalkar56@gmail.com>
2026-01-09 14:48:29 +0530

    Updated Uploads Error

--- Backend/Routes/siteRoute.js
+++ Backend/Routes/siteRoute.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const siteController = require('../Controllers/siteController');
-const upload = require('../utils/upload'); 
+
 
 router.route('/')
   .get(siteController.getSites)
-  // 🛑 THIS IS THE CRITICAL PART
-  .post(upload.fields([
-      { name: 'thumb', maxCount: 1 }, 
-      { name: 'glb', maxCount: 1 },
-      { name: 'oldSitePhoto', maxCount: 1 }, // <--- MUST BE HERE
-      { name: 'newSitePhoto', maxCount: 1 }  // <--- MUST BE HERE
-  ]), siteController.createSite);
+
+  .post(siteController.createSite); 
 
 router.route('/:id')
   .get(siteController.getSiteById)
-  .put(upload.fields([
-      { name: 'thumb', maxCount: 1 }, 
-      { name: 'glb', maxCount: 1 },
-      { name: 'oldSitePhoto', maxCount: 1 }, // <--- MUST BE HERE
-      { name: 'newSitePhoto', maxCount: 1 }  // <--- MUST BE HERE
-  ]), siteController.updateSite)
+  
+  .put(siteController.updateSite) 
   .delete(siteController.deleteSite);
 
 module.exports = router;
