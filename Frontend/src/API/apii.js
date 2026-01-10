5997bfe37c14bff65ef070b36777bd06a6b7cb4f
Parth-Chikalkar <parthchikalkar56@gmail.com>
2026-01-10 15:32:36 +0530

    Updated Err in chatbox

--- Frontend/src/API/apii.js
+++ Frontend/src/API/apii.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/holoheri/ai",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/holoheri/ai/ask`,  
   headers: {
     "Content-Type": "application/json",
   },
